0e55ed9753c2b8401630a597155c25fe768674b9
Tarun Bhardwaj <mailme@tarunbhardwaj.com>
2016-03-14 13:59:16 +0530

    Install fio-product-notebook

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ minor_version = int(minor_version)
 
 requires = []
 
-MODULE2PREFIX = {'product_notebook': 'openlabs'}
+MODULE2PREFIX = {'product_notebook': 'fio'}
 
 MODULE = "product_variant_measurements"
 PREFIX = "fio"
